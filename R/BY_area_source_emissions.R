e9078a3eba30095a70c5c31de696e0ad963446f5
David Holstius <david.holstius@gmail.com>
2020-01-17 09:52:43 -0800

    (deprecated) support for area_source_projections() and area_source_emissions()

--- R/BY_area_source_emissions.R
+++ R/BY_area_source_emissions.R
@@ -1,3 +1,12 @@
+#' @include BY_area_source_projections.R
+
+#' BY_area_source_emissions
+#'
+#' @description `BY_area_source_emissions()` returns results for a single year (the base year).
+#'
+#' @rdname BY_area_source_projections
+#' @usage BY_area_source_emissions(...)
+#'
 #' @export
 BY_area_source_emissions <- function (
   base_year,
@@ -6,8 +15,8 @@ BY_area_source_emissions <- function (
 ) {
 
   BY_area_source_emission_data <-
-    base_year %>%
     BY_area_source_projections(
+      base_year,
       years = CY(elide_year(base_year)),
       ...,
       verbose = verbose)
---
+++ R/deprecated.R
@@ -0,0 +1,15 @@
+#' @rdname BY_area_source_projections
+#' @usage NULL
+#' @export
+area_source_projections <- function (...) {
+  .Deprecated("BY_area_source_projections")
+  return(BY_area_source_projections(...))
+}
+
+#' @rdname BY_area_source_emissions
+#' @usage NULL
+#' @export
+area_source_emissions <- function (...) {
+  .Deprecated("BY_area_source_emissions")
+  return(BY_area_source_emissions(...))
+}
